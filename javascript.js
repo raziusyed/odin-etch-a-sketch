006c4d73021cd4473bbc0717d25044a71941f32c
Razi <raziusyed@gmail.com>
2025-11-19 12:19:34 -0500

    Add hover effect with event listeners

--- javascript.js
+++ javascript.js
@@ -5,8 +5,14 @@ let n = 16;
 for (let i = 0; i < n; i++) {
     let row = document.createElement("div");
     grid.appendChild(row);
+    row.classList.add("row");
 
     for (let j = 0; j < n; j++) {
-        row.appendChild(document.createElement("div"));
+        let col = document.createElement("div");
+        col.addEventListener("mouseenter", () => {
+            col.classList.add("hover");
+        })
+        row.appendChild(col);
+        col.classList.add("col");
     }
 }
